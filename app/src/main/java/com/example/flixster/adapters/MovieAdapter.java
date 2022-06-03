70ea3017c729ac0c3f051b307e22b04c5df79d7e
Tina Wen <tinawen@fb.com>
2022-06-03 11:02:46 -0700

    adding project

---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,126 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.flixster.MovieDetailsActivity;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+        // Get the movie at the passed in position
+        Movie movie = movies.get(position);
+        // Bind the movie data into the VH
+        holder.bind(movie);
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPoster = (ImageView) itemView.findViewById(R.id.ivPoster);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            // add this as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
+
+            /*
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+             */
+        }
+
+        // when the user clicks on a row, show MovieDetailsActivity for the selected movie
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            String imageUrl;
+            int placeholderID;
+
+            // if phone is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // then imageUrl = backdrop image
+                imageUrl = movie.getBackdropPath();
+                placeholderID = R.drawable.flicks_backdrop_placeholder;
+            } else {
+                // else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+                placeholderID = R.drawable.flicks_movie_placeholder;
+            }
+
+            Glide.with(context).load(imageUrl)
+                    .placeholder(placeholderID)
+                    .centerCrop()
+                    .transform(new RoundedCorners(50))
+                    .into(ivPoster);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,58 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public Movie(JSONObject movie) throws JSONException {
+        title = movie.getString("title");
+        overview = movie.getString("overview");
+        posterPath = movie.getString("poster_path");
+        backdropPath = movie.getString("backdrop_path");
+        voteAverage = movie.getDouble("vote_average");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
